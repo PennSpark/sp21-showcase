e1d103f8c3ca42fa8b77d95c30695aac665b7f5f
Grace Jiang <gracjiang@gmail.com>
2021-05-08 05:53:15 -0700

    added social media icons

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,10 +9,11 @@ export default function Layout({ children }) {
         <html lang="en">
             <head>
                 <title>PCL Showcase</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
             </head>
             <body style={{margin: "0px", padding: "0px"}}>
                 {children}
-            <footer style={{height: "50px"}}></footer>
+            <footer style={{height: "50px", marginTop: "40px", textAlign: "center", fontSize: "1rem"}}><a href="https://github.com/PennCreativeLabs/sp21-showcase" target="_blank" rel="noopener noreferrer">Source Code</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="http://penncreativelabs.com/" target="_blank" rel="noopener noreferrer">Penn Creative Labs</a>&nbsp;&nbsp;<a href="https://www.facebook.com/penncreativelabs" target="_blank" rel="noopener noreferrer" class="fa fa-facebook"/>&nbsp;<a href="https://www.instagram.com/penncreativelabs" target="_blank" rel="noopener noreferrer" class="fa fa-instagram"/></footer>
             </body>
         </html>
     )
